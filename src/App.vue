3a0af56b37955618a1e8e620b1d07b22b17388ad
一根萝卜 <1738919936@qq.com>
2023-05-17 18:51:12 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="title">认识一下，伏兵我学的</div>
+    <p class="content">Vant 是一个面向移动端的组件库，因此默认只适配了移动端设备，这意味着组件只监听了移动端的 touch 事件，没有监听桌面端的 mouse 事件。
+  如果你需要在桌面端使用 Vant，可以引入我们提供的 @vant/touch-emulator，这个库会在桌面端自动将 mouse 事件转换成对应的 touch 事件，使得组件能够在桌面端使用。</p>
+
+  <van-button type="primary">主要按钮</van-button>
+  <van-button type="success">成功按钮</van-button>
+  <van-button type="default">默认按钮</van-button>
+  <van-button type="warning">警告按钮</van-button>
+  <van-button type="danger">危险按钮</van-button>
+
+  <van-button plain hairline type="primary">细边框按钮</van-button>
+  <van-button plain hairline type="success">细边框按钮</van-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
@@ -25,4 +35,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+  .title{
+    font-size: 30px;
+    font-weight: bold;
+    text-align: center;
+  }
+  .content{
+    font-size: 24px;
+    line-height: 2;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入所有组件
+import vant from 'vant'
+import 'vant/lib/index.css'
+
 Vue.config.productionTip = false
+
+Vue.use(vant)
 
 new Vue({
   render: h => h(App),
